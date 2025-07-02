6dead18ec12cadbb635ee87d534be552f1cc0309
Anuj Mehra <anujmehra181248@gmail.com>
2025-07-02 12:27:11 +0530

    add this file

--- src/app/pages/dashboard/reports/reports.component.ts
+++ src/app/pages/dashboard/reports/reports.component.ts
@@ -1,14 +1,39 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-reports',
   standalone: true,
-  imports: [RouterOutlet,RouterModule,CommonModule],
   templateUrl: './reports.component.html',
-  styleUrl: './reports.component.less'
+  styleUrls: ['./reports.component.less'],
+  imports: [FormsModule, HttpClientModule, RouterModule, RouterOutlet],
 })
-export class ReportsComponent {
+export class reportsComponent {
+  email = '';
+  password = '';
+  message = '';
 
-}
+  constructor(private http: HttpClient) {}
+
+  login() {
+    this.http.post<any>('http://localhost:3000/api/login', {
+      email: this.email,
+      password: this.password
+    }).subscribe({
+      next: (res) => {
+        this.message = res.message;
+        console.log('✅ Login Success:', res);
+
+        // 💾 Save token in localStorage
+        localStorage.setItem('token', res.token);
+      },
+      error: (err) => {
+        this.message = err.error.message;
+        console.log('❌ Login Failed:', err);
+      }
+    });
+  } }
